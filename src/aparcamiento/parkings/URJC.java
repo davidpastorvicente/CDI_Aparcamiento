68e4935dafb6ec7260b42410e14f5ad977677510
david <david.pastor.vicente@alumnos.upm.es>
2019-03-26 12:58:55 +0100

    nuevos parkings

--- src/aparcamiento/parkings/URJC.java
+++ src/aparcamiento/parkings/URJC.java
@@ -3,18 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package aparcamiento;
+package aparcamiento.parkings;
 
 /**
  *
  * @author david
  */
-public class Principal extends javax.swing.JFrame {
+public class URJC extends javax.swing.JFrame {
 
     /**
      * Creates new form Principal
      */
-    public Principal() {
+    public URJC() {
         initComponents();
 
     }
